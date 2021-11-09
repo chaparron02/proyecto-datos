b185cb88716b838153748b2e87101cfc8cb478d1
TikkiB <82107958+TikkiB@users.noreply.github.com>
2021-11-09 11:14:51 -0500

    Update Consulta_datos.py

--- Conexion_python/Consulta_datos.py
+++ Conexion_python/Consulta_datos.py
@@ -2,66 +2,82 @@ import psycopg2
 
 try:
     conexion = psycopg2.connect(user="postgres",
-                                password="*******",
+                                password="Kalamar01",
                                 database="nacimientos",
                                 host="localhost",
                                 port="5432")
     print("Conexión correcta!")
     
     sql1 = """select *
-         from madre
-         ORDER BY 1;"""
+         from madre;"""
 
     sql2 = """select *
-         from padre
-         ORDER BY 1;"""
+         from padre;"""
 
-    sql3 = """select *
-         from bebe
-         ORDER BY 1;"""
+    sql3 = """SELECT * 
+         from bebe;"""
     
 
- #Se ejecuta la sentencia para mostrar los registros de la tabla madre
+ #Se ejecuta la sentencia para mostrar los registros de la tabla madre y se guarda en un archivo llamado madre.txt
+    
     cursor = conexion.cursor()
     cursor.execute(sql1)
     madres = cursor.fetchall()
-    print("****tabla madre***")
-    for madre in madres:
-        print(f"id_madre: {madre[0]}")
-        print(f"edad_madre: {madre[1]}")
-        print(f"embarazos: {madre[2]}")
-        print(f"hijos_vivos: {madre[3]}","\n") 
+    with open('madre.txt','w') as m:
 
-#Se ejecuta la sentencia para mostrar los registros de la tabla padre 
+        m.write("****tabla madre***" + "\n")
+
+        for madre in madres:
+            m.write(f"id_madre: {madre[0]}" +"\n")
+            m.write(f"edad_madre: {madre[1]}" +"\n")
+            m.write(f"embarazos: {madre[2]}"+"\n")
+            m.write(f"hijos_vivos: {madre[3]}"+"\n") 
+            m.write("\n")
+    
+
+ #Se ejecuta la sentencia para mostrar los registros de la tabla padre y se guarda en un archivo llamado padre.txt
+    
     cursor = conexion.cursor()
     cursor.execute(sql2)
     padres = cursor.fetchall()
-    print("****tabla padre***")
-    for padre in padres:
-        print(f"id_padre: {padre[0]}")
-        print(f"edad_padre: {padre[1]}","\n")
+
+    with open('padre.txt','w') as p:
+
+        p.write("****tabla padre***")
+        p.write('\n')
+
+        for padre in padres:
+            p.write(f"id_padre: {padre[0]}"+"\n")
+            p.write(f"edad_padre: {padre[1]}"+"\n")   
+            p.write('\n')
         
-
-#Se ejecuta la sentencia para mostrar los registros de la tabla bebe 
-
+    
+#Se ejecuta la sentencia para mostrar los registros de la tabla bebe y se guarda en un archivo llamado bebe.txt
+    
     cursor = conexion.cursor()
     cursor.execute(sql3)
     bebes = cursor.fetchall()
-    print("****tabla bebe***")
-    for bebe in bebes:
-        print(f"id_bebe: {bebe[0]}")
-        print(f"genero: {bebe[1]}")
-        print(f"peso: {bebe[2]}")
-        print(f"talla: {bebe[3]}")
-        print(f"fecha_nacimiento: {bebe[4]}")
-        print(f"tiempo_gestacion: {bebe[5]}")
-        print(f"tipo_parto: {bebe[6]}")
-        print(f"multiplicidad: {bebe[7]}")
-        print(f"apgar1: {bebe[8]}")
-        print(f"apgar2: {bebe[9]}")
-        print(f"id_madre_madre: {bebe[10]}")
-        print(f"id_padre_padre: {bebe[11]}")
 
+    with open('bebe.txt','w') as d:
+
+        d.write("tabla bebe")
+        d.write('\n')
+  
+        for bebe in bebes:
+            d.write(f"id_bebe:  {bebe[0]}" + "\n")
+            d.write(f"genero: {bebe[1]}"+ "\n")
+            d.write(f"peso: {bebe[2]}"+ "\n")
+            d.write(f"talla: {bebe[3]}"+ "\n")
+            d.write(f"fecha_nacimiento: {bebe[4]}"+ "\n")
+            d.write(f"tiempo_gestacion: {bebe[5]}"+ "\n")
+            d.write(f"tipo_parto: {bebe[6]}"+ "\n")
+            d.write(f"multiplicidad: {bebe[7]}"+ "\n")
+            d.write(f"apgar1: {bebe[8]}"+ "\n")
+            d.write(f"apgar2: {bebe[9]}"+ "\n")
+            d.write(f"id_madre_madre: {bebe[10]}"+ "\n")
+            d.write(f"id_padre_padre: {bebe[11]}"+"\n")
+            d.write("\n")
+        
     
     
 except psycopg2.Error as e:
